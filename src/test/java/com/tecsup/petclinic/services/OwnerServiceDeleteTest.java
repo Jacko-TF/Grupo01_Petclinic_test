7f057f71ca84fb5934e3dc65e2d49a2da9dc98a7
Nicole <nicole.xatira@gmail.com>
2023-06-12 01:29:00 -0500

    DeleteTest2

--- src/test/java/com/tecsup/petclinic/services/OwnerServiceDeleteTest.java
+++ src/test/java/com/tecsup/petclinic/services/OwnerServiceDeleteTest.java
@@ -19,29 +19,52 @@ public class OwnerServiceDeleteTest {
     @Autowired
     private OwnerService ownerService;
     @Test
-    public void testDeleteOwner() {
-        int OWNER_ID = 21;
+    public void testDeleteOwnerById() {
+        int OWNER_ID = 26;
         String OWNER_FIRST_NAME = "Nicole";
         String OWNER_LAST_NAME  = "Arguedas";
         String OWNER_ADRESS = "Calle Ibiza 250";
         String OWNER_CITY = "La Molina";
         String OWNER_TELEFONO = "9085551023";
 
-        Owner owner = new Owner(OWNER_ID, OWNER_FIRST_NAME,OWNER_LAST_NAME, OWNER_ADRESS, OWNER_CITY, OWNER_TELEFONO);
+        Owner owner = new Owner(OWNER_ID, OWNER_FIRST_NAME, OWNER_LAST_NAME, OWNER_ADRESS, OWNER_CITY, OWNER_TELEFONO);
         Owner ownerCreated = this.ownerService.create(owner);
 
-        log.info("OWNER CREATED :" + ownerCreated.toString());
+        log.info("OWNER CREATED: " + ownerCreated.toString());
 
         try {
-            this.ownerService.delete(owner.getId(), owner.getFirstName());
-        }catch(OwnerNotFoundException e) {
+            this.ownerService.delete(String.valueOf(owner.getId()));
+        } catch (OwnerNotFoundException e) {
             fail(e.getMessage());
         }
-        try {
+
+        assertThrows(OwnerNotFoundException.class, () -> {
             this.ownerService.findById(owner.getId());
-            assertTrue(false);
-        }catch(OwnerNotFoundException e) {
-            assertTrue(true);
+        });
+    }
+    @Test
+    public void testDeleteOwnerByName() {
+        int OWNER_ID = 16;
+        String OWNER_FIRST_NAME = "Nicole";
+        String OWNER_LAST_NAME  = "Arguedas";
+        String OWNER_ADRESS = "Calle Ibiza 250";
+        String OWNER_CITY = "La Molina";
+        String OWNER_TELEFONO = "9085551023";
+
+        Owner owner = new Owner(OWNER_ID, OWNER_FIRST_NAME, OWNER_LAST_NAME, OWNER_ADRESS, OWNER_CITY, OWNER_TELEFONO);
+        Owner ownerCreated = this.ownerService.create(owner);
+
+        log.info("OWNER CREATED: " + ownerCreated.toString());
+
+        try {
+            this.ownerService.delete(owner.getFirstName());
+        } catch (OwnerNotFoundException e) {
+            fail(e.getMessage());
         }
+
+        assertThrows(OwnerNotFoundException.class, () -> {
+            this.ownerService.findById(owner.getId());
+        });
     }
 }
+
